c0861a109f44296aa99cd0ac1d4ff2d7fbbaef55
Samrudha Kelkar <kelkarsamrudha@gmail.com>
2019-03-10 19:00:46 +0530

    Add helper

---
+++ helper.py
@@ -0,0 +1,17 @@
+def formKernel(X1, X2, gamma=None):
+    return np.exp( np.subtract( np.matmul(X1,X2.T),1) )
+
+
+
+def getKernelValueExtremes(K, lowerPct, upperPct):
+
+
+    v1 = np.sort(K, axis =None)
+
+    low_index = math.ceil(lowerPct * len(v1))
+    high_index = math.ceil(upperPct * len(v1))
+
+    l = v1[low_index];
+    u = v1[high_index]
+
+    return l, u
--- learnAsymmTransform.py
+++ learnAsymmTransform.py
@@ -4,7 +4,9 @@ Learning asymmetric transform
 
 '''
 import numpy as np
-import pandas as pd
+import math
+import helper
+
 
 
 
@@ -14,15 +16,18 @@ class Params():
     def __init__(self):
         pass
 
-def formKernel(X1, X2, gamma=None):
-    return np.exp( np.subtract( np.matmul(X1,X2.T),1) )
 
 
 
-def getKernelValueExtremes(K, lowerPct, upperPct):
+def getConstraints_InterdomainSimilarity(yA,yB,l,u);
 
-    return np.sort(K, axis =None)
 
+    pos = 1;
+    ly1 = length(y1);
+    ly2 = length(y2);
+    C = zeros(ly1 * ly2, 4)
+
+    pass
 
 
 def learnAymmTransform(XA, yA, XB, yB, params):
@@ -32,12 +37,12 @@ def learnAymmTransform(XA, yA, XB, yB, params):
 
 
     ## Form kernel matrix
-    K0train = formKernel(X, X)
+    K0train = helper.formKernel(X, X)
 
     ## Calculate lowe and upper thresholds
-    a = getKernelValueExtremes(K0train, 0.02, 0.98)
+    l, u = helper.getKernelValueExtremes(K0train, 0.02, 0.98)
 
-    return a
+    return True
 
 
 
@@ -61,8 +66,8 @@ if __name__=='__main__':
 
     params = Params()
 
-    xA = np.array(np.random([795,800]))
-    xB = np.array(np.random([498, 800]))
+    xA = np.random.rand(795,800)
+    xB = np.random.rand(498,800)
 
     yA = np.array([1]*795)
     yB = np.array([1] * 498)
